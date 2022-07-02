a89c5e0d5a10aafb9605c9e889eca79bdd408700
artidata <imaduddin.htm@gmail.com>
2022-07-02 19:39:56 +0000

    Removing CMS_NAME and change website title and blog everywhere.

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,20 +1,17 @@
-import { CMS_NAME } from '../lib/constants'
-
 const Intro = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+        Rules.
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{' '}
+        The rules of probabilistic games in{' '}
         <a
           href="https://nextjs.org/"
           className="underline hover:text-blue-600 duration-200 transition-colors"
         >
-          Next.js
-        </a>{' '}
-        and {CMS_NAME}.
+          terjago.com
+        </a>
       </h4>
     </section>
   )
